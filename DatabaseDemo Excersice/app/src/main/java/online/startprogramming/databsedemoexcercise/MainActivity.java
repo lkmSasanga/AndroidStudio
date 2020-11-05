8ae40cf4f90bb76ae8146120f0067f9f8cd554d8
lkmSasanga <lkmsasanga@gmail.com>
2020-11-05 11:05:53 +0530

    sql db example with error handling

--- DatabaseDemo Excersice/app/src/main/java/online/startprogramming/databsedemoexcercise/MainActivity.java
+++ DatabaseDemo Excersice/app/src/main/java/online/startprogramming/databsedemoexcercise/MainActivity.java
@@ -2,7 +2,10 @@ package online.startprogramming.databsedemoexcercise;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            SQLiteDatabase sqLiteDatabase = this.openOrCreateDatabase("Events", MODE_PRIVATE, null);
+            sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS events (name VARCHAR, year INT(4))");
+
+            sqLiteDatabase.execSQL("INSERT INTO events (name, year) VALUES ('MIlenieum', 2000)");
+            sqLiteDatabase.execSQL("INSERT INTO events (name, year) VALUES ('Nick started teaching', 2014)");
+
+            Cursor c = sqLiteDatabase.rawQuery("SELECT * FROM events",null);
+            int nameIndex = c.getColumnIndex("name");
+            int yearIndex = c.getColumnIndex("year");
+            c.moveToFirst();
+
+            while (c != null) {
+                Log.i("Results - name", c.getString(nameIndex));
+                Log.i("Results - year", Integer.toString(c.getInt(yearIndex)));
+
+                //to go next row in db
+                c.moveToNext();
+
+            }
+
+
+
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
